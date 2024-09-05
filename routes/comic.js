1104e8c633f4786c6958edf478201c56de1a6ada
Devlojo <jonathan.lacouture@oclock.school>
2024-09-06 01:40:03 +0200

    Work in progress

--- routes/comic.js
+++ routes/comic.js
@@ -15,4 +15,30 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+router.get("/comic/:comicId", async (req, res) => {
+  try {
+    const comicId = req.params.comicId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+router.get("/comics/:characterId", async (req, res) => {
+  try {
+    const characterId = req.params.characterId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
 module.exports = router;
